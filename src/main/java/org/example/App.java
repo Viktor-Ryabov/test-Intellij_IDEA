634842b88683ab3c492053fa4c7a7da0a8ed13a3
Viktor-Ryabov <iprva@ya.ru>
2024-09-09 13:01:27 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/org/example/App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,14 +7,5 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is test push from Intellij_IDEA");
-
-        Mountain mountain = new Mountain();
-        mountain.setName("Elbrus");
-        mountain.setHightMountain(5642);
-
-        String name = mountain.getName();
-        int hight = mountain.getHightMountain();
-
-        System.out.printf("В России есть гора %s и её высота %d метров!", name, hight);
     }
 }

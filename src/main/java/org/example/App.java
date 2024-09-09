04e65c739c9e8f2cfb30c44bb953c14b6bdd7315
Viktor-Ryabov <iprva@ya.ru>
2024-09-09 13:01:27 +0300

    added sout "This is test push from Intellij_IDEA"

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,12 +2,10 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is test push from Intellij_IDEA");
     }
 }
